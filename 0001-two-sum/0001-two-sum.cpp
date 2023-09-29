004f09395a79b037ab7421bb0da76d2f6b822483
ShashankVashishtha18 <83297481+ShashankVashishtha18@users.noreply.github.com>
2023-09-29 13:57:06 +0530

    Time: 7 ms (90.03%), Space: 11.3 MB (17.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-     
-        for(int i = 0; i< n-1; i++){
-          for(int j = i+1; j<n; j++){
-            if(nums[i] + nums[j] == target){
-              return {i , j};
+        unordered_map <int , int> mp;
+
+        for(int i=0; i<n; i++){
+            int remain = target - nums[i];
+           if (mp.find(remain) != mp.end()){
+                return {i , mp[remain]};
             }
-          }
+            mp[nums[i]] = i;
         }
-        return {};
+        throw std::invalid_argument("No solution found");
     }
 };
